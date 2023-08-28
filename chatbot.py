bc09eb08fd8ce2fe7bd1441ed38c28e24e3c5fc2
Akshath <127722008+akvk1712@users.noreply.github.com>
2023-08-28 22:57:38 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()  # Fixed the instantiation of WordNetLemmatizer
+
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))  # Corrected the filename extension
+model = load_model('chatbot_model.model')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):  # Corrected the spelling of 'enumerate'
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+
+    return return_list
+
+def get_response(intents_list):
+    tag = intents_list[0]['intent']
+    for intent in intents:
+        if intent['tag'] == tag:
+            result = random.choice(intent['responses'])
+            break
+    return result
+
+print("Chatbot is running")  # Corrected the indentation
